0b8586937789fd16154d04f53e0400164bde7f32
divyabohra072 <divya.bohra072@gmail.com>
2026-03-27 15:38:11 +0530

    Add even-odd checker in numerical2.py
    Implement a program to check if a number is even or odd.

---
+++ numerical2.py
@@ -0,0 +1,10 @@
+#Write a program to check whether a given number is even or odd using conditional statements.
+
+#Code
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+print("Divya Bohra, 2581130")
