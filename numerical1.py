c43969887cbef71d3e23c7138098def0781e4467
divyabohra072 <divya.bohra072@gmail.com>
2026-03-27 15:37:42 +0530

    Add program to find the greater of two numbers
    Implement a program to compare two numbers and print the greater one.

---
+++ numerical1.py
@@ -0,0 +1,11 @@
+#Write a Python program to take two numbers as input and print the greater number using an if-else statement.
+
+#Code  
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if a > b:
+    print(a)
+else:
+    print(b)
+print("Divya Bohra, 2581130")
